32989005f80f0389caacfad5189e1164254b5ef8
kanchanchoudhary02 <kanchan31choudhary.in>
2026-02-10 11:33:59 +0530

    riya resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,7 +4,7 @@ function Resume() {
   return (
     <section className="resume-wrapper">
       <div className="resume-card">
-        <h1>Kanchan Choudhary</h1>
+        <h1>Riya Suthar</h1>
         <p className="role">Software & Web Developer</p>
 
         <p className="resume-text">
@@ -13,7 +13,7 @@ function Resume() {
         </p>
 
         <a
-          href="/Kanchan_Choudhary_Resume.pdf"
+          href="/Riya_suthar_Resume.pdf"
           download
           className="resume-download"
         >
